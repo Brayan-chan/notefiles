901f905c4ba5e49ff089ee9282869181cccfa770
Brayan-chan <al071392@uacam.mx>
2024-11-09 17:49:46 -0600

    preparing project for productión

--- Config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-const BASE_URL = "http://localhost/notefiles/";
-const HOST = "localhost";
-const USER = "root";
-const PASS = "";
-const DB = "notefiles";
-const CHARSET = "charset=utf8";
-?>
